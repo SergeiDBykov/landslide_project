52eb0612b058a00242e4b880f910c1918887fd0d
david <david.le@inovex.de>
2022-10-01 20:50:04 +0200

    added backend skeleton

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from predict import predict
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+@app.post("/test")
+async def hello_world():
+    return {"message": f"Hello World"}
